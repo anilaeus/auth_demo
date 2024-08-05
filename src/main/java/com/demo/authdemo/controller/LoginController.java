bcba524fa2b82868aeb3338d2062ba1e3707e97b
Anıl DEMİR <anilaeus@gmail.com>
2024-08-05 15:58:56 +0300

    initial

---
+++ src/main/java/com/demo/authdemo/controller/LoginController.java
@@ -0,0 +1,32 @@
+package com.demo.authdemo.controller;
+
+
+import com.demo.authdemo.entity.User;
+import com.demo.authdemo.requests.LoginRequest;
+import com.demo.authdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest user) {
+        boolean success = userService.validateUser(user);
+        if (success) {
+            return ResponseEntity.ok("Login Successful");
+        } else {
+            return ResponseEntity.badRequest().body("Login Failed");
+        }
+    }
+
+
+}
---
+++ src/main/java/com/demo/authdemo/service/UserService.java
@@ -0,0 +1,25 @@
+package com.demo.authdemo.service;
+
+import com.demo.authdemo.entity.User;
+import com.demo.authdemo.repository.UserRepository;
+import com.demo.authdemo.requests.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public boolean validateUser(LoginRequest request) {
+        Optional<User> optUser = userRepository.findByUsername(request.getUsername());
+        return optUser.isPresent();
+    }
+
+    public void createUser(User user){
+        userRepository.save(user);
+    }
+}
